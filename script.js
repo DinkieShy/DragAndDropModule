13bd841f778588908583c2fdb2520d92966bfbbf
Dinkie <david.churchill72@gmail.com>
2019-07-19 14:03:01 +0100

    Change colours on drag and drop after check and sort based on correct

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ var dropAreas = [dropArea1, dropArea2, dropArea3];
 var itemBeingDragged;
 var correctDropArea1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 var correctDropArea2 = [13, 14, 15, 16, 17];
-var correctDropArea3 = [18, 19, 20, 21, 22, 23, 24, 25];
-var amountOfItems = 25;
+var correctDropArea3 = [18, 19, 20, 21, 22, 23, 24];
+var amountOfItems = 24;
 var correctDropAreas = [correctDropArea1, correctDropArea2, correctDropArea3];
 
 $(document).ready(function(){
@@ -53,7 +53,6 @@ function inIframe(){
 }
 
 function checkAnswers(){
-  console.log("Checking answers...");
   var correct = true;
   dropAreas[0] = [];
   dropAreas[1] = [];
@@ -80,16 +79,26 @@ function checkAnswers(){
     for(var ii = 0; ii < dropAreas[i].length; ii++){
       if(correctDropAreas[i].includes(dropAreas[i][ii], 0)){
         $('#item' + dropAreas[i][ii]).addClass("correct");
+        $('#item' + dropAreas[i][ii]).attr('data-sort', 1);
         $('#item' + dropAreas[i][ii]).removeClass("incorrect");
-        console.log(`Item ${dropAreas[i][ii]} correct`);
       }
       else{
         $('#item' + dropAreas[i][ii]).addClass("incorrect");
-        console.log(`Item ${dropAreas[i][ii]} incorrect`);
+        $('#item' + dropAreas[i][ii]).attr('data-sort', 0);
         $('#item' + dropAreas[i][ii]).removeClass("correct");
         correct = false;
       }
     }
+  }
+  if(!correct){
+    sortDropAreas();
+    $('.dragItem').draggable({
+      cursor: "move",
+      revert: 'invalid',
+      helper: 'clone',
+      appendTo: 'body',
+      scroll: false
+    });
   }
   if(correct && unusedItems.length == 0){
     showEndScreen();
@@ -99,6 +108,38 @@ function checkAnswers(){
     $('#tryAgainScreen').animate({ left: '10%' }, 1000, function() {
       setTimeout(hideTryAgainScreen, 3000);
     });
+  }
+}
+
+function sortDropAreas(){
+  dropArea1 = $('#drop1 .dragItem').toArray().sort(function(a, b){
+    return parseInt(a.dataset.sort) - parseInt(b.dataset.sort);
+  });
+
+  dropArea2 = $('#drop2 .dragItem').toArray().sort(function(a, b){
+    return parseInt(a.dataset.sort) - parseInt(b.dataset.sort);
+  });
+
+  dropArea3 = $('#drop3 .dragItem').toArray().sort(function(a, b){
+    return parseInt(a.dataset.sort) - parseInt(b.dataset.sort);
+  });
+
+  drop1Element = $('#drop1')[0];
+  drop1Element.innerHTML = "";
+  for(var i = 0; i < dropArea1.length; i++){
+    drop1Element.innerHTML += dropArea1[i].outerHTML;
+  }
+
+  drop2Element = $('#drop2')[0];
+  drop2Element.innerHTML = "";
+  for(var i = 0; i < dropArea2.length; i++){
+    drop2Element.innerHTML += dropArea2[i].outerHTML;
+  }
+
+  drop3Element = $('#drop3')[0];
+  drop3Element.innerHTML = "";
+  for(var i = 0; i < dropArea3.length; i++){
+    drop3Element.innerHTML += dropArea3[i].outerHTML;
   }
 }
 
